4767d61132ebf2b9080ef350c92d5ac5d4723e41
Tatsuya Yamashita <yamashitata@ikyu.com>
2022-09-07 15:13:32 +0900

    abc098b ac

--- abc098/b/main.js
+++ abc098/b/main.js
@@ -1,35 +1,26 @@
-const { assert } = require("console");
-
-function main() {
-  let input = require("fs").readFileSync("/dev/stdin", "utf8");
+"use strict";
+function main(input) {
   let lines = input.split("\n");
   let n = parseInt(lines[0], 10);
   let s = lines[1];
-  assert(s.length === n);
 
   let answer = 0;
-  let commons = new Set();
+  let commons = [];
   for (let i = 1; i < n; i++) {
     let x = s.slice(0, i);
     let y = s.slice(i, n);
     //console.log(`x: ${x}, y: ${y}`);
-    commons.clear();
-
+    commons = [];
     for (let k = 0; k < x.length; k++) {
-      for (let l = k + 1; l <= x.length; l++) {
-        let z = x.slice(k, l);
-        //console.log(`  k: ${k}, l: ${l}, z: ${z}`);
-        if (y.includes(z) && !commons.has(z)) {
-          commons.add(z);
-        } else {
-          continue;
-        }
+      let z = x[k];
+      if (y.includes(z) && !commons.includes(z)) {
+        commons.push(z);
       }
     }
-    answer = answer < commons.size ? commons.size : answer;
+    answer = answer < commons.length ? commons.length : answer;
   }
 
   console.log(answer);
 }
 
-main();
+main(require("fs").readFileSync("/dev/stdin", "utf8"));
